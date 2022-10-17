d736947917f6b2ff762d9b9b98204f6548a70d2e
Elany Ferreira de Souza <elany.ferreira@seati.ma.gov.br>
2022-10-17 14:44:51 -0300

    Funcao calculo idade

---
+++ functionIdade.php
@@ -0,0 +1,39 @@
+<?php
+
+//CALCULA IDADE DO USUÁRIO
+function calcularIdade($data){
+
+    $idade = 0;
+
+    //converte para o formato data
+    $data_nascimento = date('Y-m-d', strtotime($data));
+
+       //separa a string transformando em array
+       //usa o hifen como parametro para separar a string
+       $data = explode("-",$data_nascimento);
+       $anoNasc    = $data[0];
+       $mesNasc    = $data[1];
+       $diaNasc    = $data[2];
+    
+       $anoAtual   = date("Y");
+       $mesAtual   = date("m");
+       $diaAtual   = date("d");
+    
+       $idade      = $anoAtual - $anoNasc; 
+
+       if ($mesAtual < $mesNasc){
+           $idade -= 1;
+
+       } 
+       elseif ( ($mesAtual == $mesNasc) && ($diaAtual <= $diaNasc) ){
+           $idade -= 1;
+       }
+    
+    return $idade;
+}
+
+
+$data_nascimento = '2000-08-28';
+
+echo calcularIdade($data_nascimento);
+?>
